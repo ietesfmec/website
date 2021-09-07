f9d2a024a4f25ad46dc3b0a6154c1d771e7d09f5
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2021-09-07 09:00:17 +0530

    minor mobile

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
     <link rel="preconnect" href="http://www.w3.org"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet"/>
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=1" />
   </Head>
   <Component {...pageProps} />
     <Navbar/>
