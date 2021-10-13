301788ceb186651936daf6072cc9592adb8e1b14
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2021-10-14 01:12:42 +0530

    optimization

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,10 @@ function MyApp({ Component, pageProps }) {
   return (
   <>
   <Head>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="http://www.w3.org"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-    <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400&display=swap" rel="stylesheet"/>
     <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=1" />
     <title>IETE SF MEC</title>
     {/* <meta name="description" content="Official website of IETE-SF-MEC" /> */}
