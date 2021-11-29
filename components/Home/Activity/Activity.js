88b30a524e71c26cf4646d6492876e2ca7a7c7bf
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2021-11-30 01:31:46 +0530

    optimizations

--- components/Home/Activity/Activity.js
+++ components/Home/Activity/Activity.js
@@ -153,8 +153,8 @@ export default function Activity() {
 
 
     function handleGesture() {
-    if (touchendX < (touchstartX - 150)) forwards()
-    if (touchendX > (touchstartX + 150)) backwards()
+    if (touchendX < (touchstartX - 81)) forwards()
+    if (touchendX > (touchstartX + 81)) backwards()
     }
 
     const handleTouchStart = (e) => {
