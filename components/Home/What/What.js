1b4a33bad98a8ec5cdca83d548ff7968e56b9f01
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2021-12-02 18:11:00 +0530

    update

--- components/Home/What/What.js
+++ components/Home/What/What.js
@@ -24,7 +24,7 @@ export default function What() {
                         <p className = { `animate__animated animate__fadeInRight` }>
                             {/* The Institution of Electronics and Telecommunication Engineers (IETE) is a leading recognized professional society focused on the growth, development and empowerment of youth through technical education and skill development in all disciplines of engineering. */}
                             
-                            IETE SF MEC is the official student’s forum of IETE at Government Model Engineering College, Kochi. With articles, webinars, training and many other interesting activities, IETE SF MEC is growing rapidly and is turning into one of the most recognized institutes in the state.
+                            IETE SF MEC is the official student’s forum of IETE at Government Model Engineering College, Thrikkakara. With articles, webinars, training and many other interesting activities, IETE SF MEC is growing rapidly and is turning into one of the most recognized institutes in the state.
                         </p>
                     </div>
                 </section>
